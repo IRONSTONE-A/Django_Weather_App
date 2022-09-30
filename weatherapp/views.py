6b2695e67fd3fafbe536a2a66eec26c376ffc4c2
6019thinkingman <99737495+6019thinkingman@users.noreply.github.com>
2022-09-30 13:58:56 +0300

    updated

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404, redirect
 from decouple import config
 import requests
 from pprint import pprint
@@ -34,7 +34,7 @@ def index(request):
         response = requests.get(url)
         content = response.json()
         data = {
-        "city" : content["name"],
+        "city" : city,
         "temp" : content["main"]["temp"],
         "icon" : content["weather"][0]["icon"],
         "desc" : content["weather"][0]["description"]
@@ -49,3 +49,9 @@ def index(request):
     return render(request, "weatherapp/index.html", context)
 
 
+def delete_city(request, id):
+    # city = City.objects.get(id=id)
+    city = get_object_or_404(City, id=id)
+    city.delete()
+    messages.warning(request, "City deleted.")
+    return redirect("home")
